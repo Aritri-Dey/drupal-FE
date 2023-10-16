c0807a17b4990a502a92416e7993348c6302e5d3
Aritri <aritri.dey@innoraft.com>
2023-10-16 18:59:27 +0530

    FT2023-000: 2nd half.
    Tried fixing slider.

--- web/themes/custom/lilac/js/custom.js
+++ web/themes/custom/lilac/js/custom.js
@@ -1,31 +1,38 @@
-$('.view-match-block .view-content').slick({
-  infinite: false,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  arrows: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-    breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
+(function ($, Drupal, once) {
+  Drupal.behaviors.customslider = {
+    attach: function (context, settings) {
+      var slider = $('.main-slider', context);
+      $(once('show-slider', slider, context)).each(function() {
+        const slides = document.querySelectorAll('.slide'); 
+        var counter = 0; 
+        slides.forEach(
+          (slide,index) => {
+            slide.style.left = `${index * 100}%`
+          }
+        )
+
+
+        const goPrev = () => {
+          counter--;
+          slideImage();
+          console.log("prev");
+        }
+
+        const goNext = () => {
+          counter++;
+          slideImage();
+          console.log("next");
+
+        }
+
+        const slideImage = () => {
+          slides.forEach(
+            (slide) => {
+              slide.style.transform = `translateX(-${counter * 100}%)`
+            }
+          )
+        }
+      });
     }
-  ]
-// });
-})(jQuery);
+  };
+})(jQuery, Drupal, once);
--- web/themes/custom/lilac/js/slider.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.slick-demo').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-    });
-  });
